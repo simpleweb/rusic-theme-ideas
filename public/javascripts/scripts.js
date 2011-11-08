dcd9801596aef2fdf6c57846c4ce137f35933b64
Paul Springett <paul@simpleweb.co.uk>
2011-11-08 18:34:16 +0000

    Fixed countdown

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -3,25 +3,28 @@
 
 $(document).ready(function(){
 
-        $("#new-idea").hide();
-        $(".showbtn").show();
+  $("#new-idea").hide();
+  $(".showbtn").show();
 
 	$('.showbtn').click(function(){
 	$("#new-idea").slideToggle();
 	});
 
-});
-
-
-// Show & hide new idea container
-
-$(document).ready(function(){
-
-        $(".slide").hide();
-        $(".show").show();
+  $(".slide").hide();
+  $(".show").show();
 
 	$('.show').click(function(){
 	   $(this).next(".slide").slideToggle();
 	});
+	
+	
+	var $time = $("time#expires-at"),
+      timestamp = Date.parse($time.attr('datetime')),
+      expiresAt = new Date(timestamp);
+      
+  $time.countdown({
+    until: expiresAt,
+    format: 'dHMS'
+  });
 
 });
