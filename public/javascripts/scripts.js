865fcfc306f1484eeebb21b96b746b167aef038c
Dale Williams <dale@simpleweb.co.uk>
2012-01-31 17:09:36 +0000

    ajax like buttons

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -24,6 +24,24 @@ $(document).ready(function(){
 
     });
 
+    // AJAXIFY LIKE BUTTONS
+function attachLiking() {
+    
+    $('a.like', '.actions-menu').on('click', function() {
+
+        var likeLink = $(this);
+
+        $.post(likeLink.attr('href'), function(data) {
+          $('body').append('<div class="alert-message success prompt">Thanks for liking the picture!</div>');
+          likeLink.replaceWith('<span class="liked">' + (parseInt(likeLink.html())+1) + '</span>');
+          removeAlerts();
+        });
+
+        return false;
+    });
+
+  }
+
     // Fade in login prompt
     $('.ideas-index .login-prompt').hide().delay(1200).fadeIn(1000);
     $('.alert-message.error, .alert-message.warning, .alert-message.success').delay(2000).slideUp('slow');
